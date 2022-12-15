14f8e145ba5b25d421319015542e967956789c31
Roman Korenev <romengrus@gmail.com>
2022-12-15 20:36:10 +0200

    Task is done

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,61 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.persistence.NoResultException;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+        User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+        User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+        User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+        User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+
+        Car car1 = new Car("Honda", 250);
+        Car car2 = new Car("Opel", 120);
+        Car car3 = new Car("Mercedes", 600);
+
+        user1.setCar(car1);
+        user2.setCar(car2);
+        user3.setCar(car3);
+
+        userService.add(user1);
+        userService.add(user2);
+        userService.add(user3);
+        userService.add(user4);
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Car = " + user.getCar());
+            System.out.println("=================================");
+        }
+
+        System.out.println("Get user with car");
+        System.out.println(userService.getUserByCar(car1));
+        System.out.println("=================================");
+
+        System.out.println("If nothing is found");
+        try {
+            User nobody = userService.getUserByCar(new Car("Some car", 123));
+        } catch (NoResultException e) {
+            System.out.println("User not found");
+        }
+
+        context.close();
+    }
+}
